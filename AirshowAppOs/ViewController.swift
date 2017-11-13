be246d9b9ee33272687c59bbafa95e080bc3d8fc
Dev Lab Mac 2 <devlabmac2@devs-imac.lan>
2017-11-13 14:02:08 -0600

    Initial Commit

---
+++ AirshowAppOs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirshowAppOs
+//
+//  Created by Dev Lab Mac 2 on 11/13/17.
+//  Copyright © 2017 Dev Lab Mac 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
